18b3f95b05d5e210665f205da67b0fb633bc6f36
Ravi Pal <ravikantpal123@gmail.com>
2019-07-07 00:44:09 +0530

    provided keys

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,11 @@
-module.exports = {
-    mongoURI : `mongodb+srv://ravi:Asdzxc123@cluster0-3mcux.mongodb.net/devconnector?retryWrites=true&w=majority`,
-    secretOrKey: 'secret'
-};
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys_prod')
+} else {
+    module.exports = require('./keys_dev')
+}
+
+
+// module.exports = {
+//     mongoURI : `mongodb+srv://ravi:Asdzxc123@cluster0-3mcux.mongodb.net/devconnector?retryWrites=true&w=majority`,
+//     secretOrKey: 'secret'
+// };
