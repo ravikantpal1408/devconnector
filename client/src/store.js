b7455b50882142f523de61aea465c0f5aa2a1a62
ravikantpal1408 <ravikantpal123@gmail.com>
2019-07-07 13:59:31 +0530

    test567

--- client/src/store.js
+++ client/src/store.js
@@ -8,7 +8,7 @@ const middleware = [thunk];
 const store = createStore(rootReducer, intialState,
     compose(
         applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     )
 );
 
